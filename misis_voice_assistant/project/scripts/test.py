1ef47e95f238513ba35aba20f5ab49b468cc5855
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2021-09-26 14:22:05 +0300

    Delete misis_voice_assistant/project directory

--- misis_voice_assistant/project/scripts/test.py
+++
@@ -1,5 +0,0 @@
-import json
-commands = json.loads(open('../assets/intents_json/intents_and_answers.json', encoding='utf-8').read())
-
-with open('../assets/intents_json/intents_and_answers.json', 'w') as json_file:
-    json.dump(commands, json_file, ensure_ascii=False)

3a5737759913aeeefa289b7543edcd8a53d7bfb8
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2021-09-27 22:57:10 +0300

    Delete scripts directory

--- scripts/test.py
+++
@@ -1,5 +0,0 @@
-from spider import WebSpider
-
-method_list = [func for func in dir(WebSpider) if callable(getattr(WebSpider, func))]
-method_list = [x for x in method_list if '__' not in x]
-print(method_list)
